4a5a502fe31256f59698f85f5ee1d43b398ba881
thagberg <timothy.m.hagberg@gmail.com>
2014-01-28 20:15:05 -0600

    Wired up rule definition editor

--- tools/rule_definition.py
+++ tools/rule_definition.py
@@ -18,8 +18,8 @@ class Rule(TextListItem):
         self.refresh_text()
 
     def refresh_text(self):
-        t = '{self.name} / {self.operator} / {self.value}'
-        self.text = t.format(self)
+        t = '{self.name} / {self.operator.text} / {self.value}'
+        self.text = t.format(self=self)
 
 
 class Operator(TextListItem):
@@ -36,15 +36,7 @@ class RuleDefinitionFrame(EditorFrame):
         self.text = 'Rule Definition'
         self.rules = self.context['rules']
         self.operators = ListItemCollection()
-        self.operators.append(Operator('lt'))
-        '''self.operators = ListItemCollection([Operator('lt'),
-                                             Operator('le'),
-                                             Operator('eq'),
-                                             Operator('ne'),
-                                             Operator('ge'),
-                                             Operator('gt'),
-                                             Operator('in')])'''
-        self.operators = ListItemCollection()
+
         # define widgets
         self.rule_list = ScrolledList(200, 300, self.rules)
         self.add_button = Button('Add Rule')
@@ -54,8 +46,9 @@ class RuleDefinitionFrame(EditorFrame):
         self.name_entry = Entry()
         self.operator_label = Label('Operators')
         self.operator_list = ScrolledList(50, 100, self.operators)
-        self.value_label = Label('Rule Label')
+        self.value_label = Label('Rule Value')
         self.value_entry = Entry()
+        self.operator_list.child.update_items()
 
         # build widget list
         append = self.widgets.append
@@ -88,3 +81,65 @@ class RuleDefinitionFrame(EditorFrame):
         self.remove_button.sensitive = False
         self.rule_list.selectionmode = SELECTION_SINGLE
         self.operator_list.selectionmode = SELECTION_SINGLE
+
+        # wire up GUI
+        self.add_button.connect_signal(SIG_CLICKED, self._add_rule)
+        self.update_button.connect_signal(SIG_CLICKED, self._update_rule)
+        self.remove_button.connect_signal(SIG_CLICKED, self._remove_rule)
+        self.rule_list.connect_signal(SIG_SELECTCHANGED, self._set_current_rule)
+        self.rule_list.connect_signal(SIG_SELECTCHANGED, self._activate_controls)
+        self.operator_list.connect_signal(SIG_SELECTCHANGED, self._activate_controls)
+
+    def _add_rule(self):
+        selected_op = self.operator_list.get_selected()[0]
+        rule_name = self.name_entry.text
+        rule_value = self.value_entry.text
+        new_rule = Rule(rule_name, selected_op, rule_value)
+        self.rule_list.items.append(new_rule)
+        self.context['rules'].append(new_rule)
+        self.context['components'].append(new_rule)
+        self.rule_list.child.update_items()
+
+    def _update_rule(self):
+        selected_rule = self.rule_list.get_selected()[0]
+        selected_rule.name = self.name_entry.text
+        selected_rule.operator = self.operator_list.get_selected()[0]
+        selected_rule.value = self.operator_entry.text
+        selected_rule.refresh_text()
+        self.rule_list.update_items()
+
+    def _remove_rule(self):
+        selected_rule = self.rule_list.get_selected()[0]
+        self.rule_list.items.remove(selected_rule)
+        self.context['rules'].remove(selected_rule)
+        self.context['components'].remove(selectd_rule)
+        self.rule_list.child.update_items()
+
+    def _set_current_rule(self):
+        pass
+
+    def _activate_controls(self):
+        selected_rule = self.rule_list.get_selected()
+        selected_operator = self.operator_list.get_selected()
+        if len(selected_rule) > 0:
+            self.update_button.sensitive = True
+            self.remove_button.sensitive = True
+        else:
+            self.update_button.sensitive = False
+        if len(selected_operator) > 0:
+            self.add_button.sensitive = True
+        else:
+            self.add_button.sensitive = False
+            self.remove_button.sensitive = False
+
+    def activate(self):
+        super(RuleDefinitionFrame, self).activate()
+        append = self.operator_list.items.append
+        append(Operator('lt'))
+        append(Operator('le'))
+        append(Operator('eq'))
+        append(Operator('ne'))
+        append(Operator('ge'))
+        append(Operator('gt'))
+        append(Operator('in'))
+        self.operator_list.child.update_items()
